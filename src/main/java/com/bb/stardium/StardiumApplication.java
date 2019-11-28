ad3e14942775685e9b27458ac4979827e54b5876
andole87 <andole87@gmail.com>
2019-11-28 15:23:51 +0900

    setting: 프로젝트 세팅

---
+++ src/main/java/com/bb/stardium/StardiumApplication.java
@@ -0,0 +1,13 @@
+package com.bb.stardium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StardiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StardiumApplication.class, args);
+	}
+
+}
